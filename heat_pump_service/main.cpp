9d6751c5740105612797e90e39f24698c6a88755
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-05-02 20:32:19 +0300

    Initialize HeatPumpService and run

--- heat_pump_service/main.cpp
+++ heat_pump_service/main.cpp
@@ -1,8 +1,43 @@
-#include <iostream>
+#include <cstdint>
+#include <cstdlib>
+#include <string>
 
-#include <grpc++/grpc++.h>
-#include <modbus.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+
+#include "communicator/modbus_tcp_communicator.h"
+#include "service/service.h"
+
+std::string ReadEnvironmentVariable(const std::string& key) {
+  char* value = std::getenv(key.c_str());
+  return value != nullptr ? std::string{value} : std::string{""};
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  const std::string kHeatPumpIp = ReadEnvironmentVariable("HEAT_PUMP_IP");
+  if (kHeatPumpIp.empty()) {
+    throw std::runtime_error{"HEAT_PUMP_IP environment variable is not set"};
+  }
+
+  const int32_t kHeatPumpPort = []() {
+    const std::string kHeatPumpPortStr = ReadEnvironmentVariable("HEAT_PUMP_PORT");
+    if (kHeatPumpPortStr.empty()) {
+      throw std::runtime_error{"HEAT_PUMP_PORT environment variable is not set"};
+    }
+    try {
+      return std::stoi(kHeatPumpPortStr);
+    } catch (const std::invalid_argument&) {
+      throw std::runtime_error{"HEAT_PUMP_PORT environment variable is not a valid port"};
+    }
+  }();
+
+  service::HeatPumpService service{
+      std::make_unique<communicator::ModbusTCPCommunicator>(kHeatPumpIp, kHeatPumpPort)};
+
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  grpc::ServerBuilder builder{};
+  builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server{builder.BuildAndStart()};
+  server->Wait();
 }
