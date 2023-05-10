9d576260ec722557f34cda05c0bdb53076df10aa
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-05-10 18:46:23 +0300

    Add contracts project for the API interface

---
+++ heating_service/HeatingService.Contracts/HeatPump/TankLimitsResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace HeatingService.Contracts.HeatPump;
+
+public record TankLimitsResponse(
+  float LowerTankMinimum,
+  float LowerTankMaximum,
+  float UpperTankMinimum,
+  float UpperTankMaximum);
