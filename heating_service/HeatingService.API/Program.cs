3895e2f3126aac61170b0eb270766009961e8e1f
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-05-11 18:55:22 +0300

    Add CORS policy

--- heating_service/HeatingService.API/Program.cs
+++ heating_service/HeatingService.API/Program.cs
@@ -2,28 +2,35 @@ using HeatingService.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddGrpcClient<HeatPumpSvc.HeatPumpSvcClient>(o => {
     o.Address = new Uri("http://host.docker.internal:50051");
 });
 builder.Services.AddScoped<IHeatPumpService, HeatPumpService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowSpecificOrigin", corsPolicyBuilder => {
+         corsPolicyBuilder
+            .WithOrigins("http://localhost:8000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
