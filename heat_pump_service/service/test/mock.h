81bbe3fb0cdb6e3032e5934afb4f1b12a18ec489
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-05-15 21:23:32 +0300

    Add missing mock method

--- heat_pump_service/service/test/mock.h
+++ heat_pump_service/service/test/mock.h
@@ -7,8 +7,10 @@
 class MockCommunicator : public communicator::ICommunicator {
  public:
   MOCK_METHOD(uint32_t, ActiveCircuitCount, (), (const, override));
-  MOCK_METHOD(communicator::Temperatures, ReadTemperatures, (), (const, override));
+  MOCK_METHOD(communicator::Temperatures, ReadTemperatures, (),
+              (const, override));
   MOCK_METHOD(communicator::TankLimits, ReadTankLimits, (), (const, override));
   MOCK_METHOD(bool, IsCompressorActive, (), (const, override));
   MOCK_METHOD(bool, IsSchedulingEnabled, (), (const, override));
+  MOCK_METHOD(void, WriteActiveCircuitCount, (uint32_t count), (override));
 };
