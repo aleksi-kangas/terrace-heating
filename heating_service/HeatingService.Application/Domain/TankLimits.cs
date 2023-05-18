284d84ac4950cccef69e864e7362bc5c899ad690
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-05-18 18:48:32 +0300

    Perform migration for adjusted tank limit values

--- heating_service/HeatingService.Application/Domain/TankLimits.cs
+++ heating_service/HeatingService.Application/Domain/TankLimits.cs
@@ -1,7 +1,11 @@
 namespace HeatingService.Application.Domain; 
 
 public record TankLimits(
-  UInt32 LowerTankMinimum,
-  UInt32 LowerTankMaximum,
-  UInt32 UpperTankMinimum,
-  UInt32 UpperTankMaximum);
+  Int32 LowerTankMinimum,
+  Int32 LowerTankMinimumAdjusted,
+  Int32 LowerTankMaximum,
+  Int32 LowerTankMaximumAdjusted,
+  Int32 UpperTankMinimum,
+  Int32 UpperTankMinimumAdjusted,
+  Int32 UpperTankMaximum,
+  Int32 UpperTankMaximumAdjusted);
--- heating_service/HeatingService.Contracts/HeatPump/TankLimitsResponse.cs
+++ heating_service/HeatingService.Contracts/HeatPump/TankLimitsResponse.cs
@@ -1,7 +1,11 @@
 ﻿namespace HeatingService.Contracts.HeatPump;
 
 public record TankLimitsResponse(
-  UInt32 LowerTankMinimum,
-  UInt32 LowerTankMaximum,
-  UInt32 UpperTankMinimum,
-  UInt32 UpperTankMaximum);
+  Int32 LowerTankMinimum,
+  Int32 LowerTankMinimumAdjusted,
+  Int32 LowerTankMaximum,
+  Int32 LowerTankMaximumAdjusted,
+  Int32 UpperTankMinimum,
+  Int32 UpperTankMinimumAdjusted,
+  Int32 UpperTankMaximum,
+  Int32 UpperTankMaximumAdjusted);
