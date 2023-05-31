134bf06006af2d0e98ce917361a59c531159a6e1
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-05-31 22:22:21 +0300

    Implemented DashboardGraph

--- ui/src/app/components/dashboard/dashboard_graph.tsx
+++ ui/src/app/components/dashboard/dashboard_graph.tsx
@@ -1,39 +1,81 @@
 'use client';
 
-import {Chart as ChartJS, registerables} from 'chart.js';
+import {
+  Chart as ChartJS,
+  ChartData,
+  ChartOptions,
+  registerables,
+} from 'chart.js';
+import 'chartjs-adapter-luxon';
 import {useMemo} from 'react';
 import {Line} from 'react-chartjs-2';
+import {DateTime, Duration} from 'luxon';
 
 ChartJS.register(...registerables);
 
 interface DashboardGraphProps {
   className?: string;
-  x: Date[];
-  y: number[];
-  yLabel: string;
+  dateTimes: string[];
+  values: number[];
+  label: string;
 }
 
 const DashboardGraph = ({
   className,
-  x,
-  y,
-  yLabel,
+  dateTimes,
+  values,
+  label,
 }: DashboardGraphProps): React.JSX.Element => {
-  const styles = 'm-2';
+  const styles = 'm-4';
   className = className ? styles.concat(' ', className) : styles;
-  const data = useMemo(
+  const data: ChartData<'line'> = useMemo(
     () => ({
-      labels: x,
+      labels: dateTimes.map(dt => DateTime.fromISO(dt).toLocal()),
       datasets: [
         {
-          label: yLabel,
-          data: y,
+          backgroundColor: 'rgb(110, 160, 176)',
+          borderColor: 'rgb(110, 160, 176)',
+          data: values,
+          label: label,
+          pointHitRadius: 5,
+          pointRadius: 2,
+          showLine: true,
+          spanGaps: false,
         },
       ],
     }),
-    [x, y, yLabel]
+    [dateTimes, values, label]
   );
-  const options = useMemo(() => ({}), []);
+  const options: ChartOptions<'line'> = useMemo(
+    () => ({
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          // Disable legend interactivity
+          onClick: () => {},
+          position: 'bottom',
+        },
+      },
+      responsive: true,
+      scales: {
+        x: {
+          min: DateTime.now()
+            .minus(Duration.fromObject({days: 2}))
+            .toMillis(),
+          max: DateTime.now().toMillis(),
+          ticks: {
+            major: {enabled: true},
+          },
+          time: {
+            displayFormats: {day: 'EEE dd', hour: 'HH:mm'},
+            tooltipFormat: 'yyyy-MM-dd EEE HH:mm',
+          },
+          type: 'time',
+        },
+      },
+    }),
+    []
+  );
   return <Line className={className} data={data} options={options} />;
 };
 
--- ui/src/app/dashboard/page.tsx
+++ ui/src/app/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import Card from '../components/card';
 import DashboardGraph from '../components/dashboard/dashboard_graph';
 import {fetchRecords} from '../api/heating';
+import {DateTime, Duration} from 'luxon';
 
 const Dashboard = async () => {
-  const to = new Date();
-  const from = new Date(to.getFullYear(), to.getMonth(), to.getDate() - 7);
+  const from = DateTime.utc().minus(Duration.fromObject({days: 2}));
+  const to = DateTime.utc();
   const records = await fetchRecords(from, to);
   return (
     <div className="flex flex-1 max-w-7xl">
@@ -12,30 +13,19 @@ const Dashboard = async () => {
         <Card className="flex-1 col-span-full md:col-span-2 flex flex-col justify-between items-center">
           <DashboardGraph
             className="max-h-72"
-            x={records.map(r => r.time)}
-            y={records.map(r => r.temperatures.outside)}
-            yLabel="Outside"
+            dateTimes={records.map(r => r.time)}
+            values={records.map(r => r.temperatures.outside)}
+            label="Outside °C"
           />
           <DashboardGraph
             className="max-h-72"
-            x={records.map(r => r.time)}
-            y={records.map(r => r.temperatures.circuit3)}
-            yLabel="Circuit 3"
+            dateTimes={records.map(r => r.time)}
+            values={records.map(r => r.temperatures.circuit3)}
+            label="Circuit 3"
           />
         </Card>
         <Card className="flex-1 col-span-full md:col-span-1 flex-col justify-between items-center">
-          <DashboardGraph
-            className="max-h-72"
-            x={records.map(r => r.time)}
-            y={records.map(r => r.temperatures.circuit3)}
-            yLabel="Circuit 1"
-          />
-          <DashboardGraph
-            className="max-h-72"
-            x={records.map(r => r.time)}
-            y={records.map(r => r.temperatures.circuit3)}
-            yLabel="Circuit 2"
-          />
+          TODO...
         </Card>
       </div>
     </div>
