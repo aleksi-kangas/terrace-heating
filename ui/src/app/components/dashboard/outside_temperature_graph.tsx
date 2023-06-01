945814425e0fb5583e16bc1cc73a7462038c10f7
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-06-01 21:53:57 +0300

    Wrap outside temperature graph in a component

---
+++ ui/src/app/components/dashboard/outside_temperature_graph.tsx
@@ -0,0 +1,24 @@
+import {DateTime, Duration} from 'luxon';
+import {fetchRecords} from '../../api/heating';
+import DashboardGraph from './dashboard_graph';
+import {Suspense} from 'react';
+import Spinner from '../spinner';
+
+const OutsideTemperatureGraph = async () => {
+  const from = DateTime.utc().minus(Duration.fromObject({days: 2}));
+  const to = DateTime.utc();
+  const records = await fetchRecords(from, to);
+
+  return (
+    <Suspense fallback={<Spinner className="flex-1 max-h-96 w-full" />}>
+      <DashboardGraph
+        className="flex-1 max-h-96 w-full"
+        dateTimes={records.map(r => r.time)}
+        values={records.map(r => r.temperatures.outside)}
+        label="Outside °C"
+      />
+    </Suspense>
+  );
+};
+
+export default OutsideTemperatureGraph;
