2dc04aadc7f50f6484c868b1a26a13e6e6687566
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-06-01 21:55:05 +0300

    Implement Dashboard loading layout and update page

--- ui/src/app/dashboard/loading.tsx
+++ ui/src/app/dashboard/loading.tsx
@@ -1,15 +1,13 @@
 import Card from '../components/card';
-import OutsideTemperatureGraph from '../components/dashboard/outside_temperature_graph';
+import Spinner from '../components/spinner';
 
-const Dashboard = () => {
+const DashboardLoading = () => {
   return (
     <div className="flex flex-1 max-w-7xl">
       <div className="flex-1 grid grid-cols-1 md:grid-cols-3 gap-4">
         <Card className="col-span-full md:col-span-2 flex flex-col justify-between items-center">
-          {/* @ts-expect-error Async Server Component */}
-          <OutsideTemperatureGraph />
-          {/* @ts-expect-error Async Server Component */}
-          <OutsideTemperatureGraph />
+          <Spinner className="flex-1 max-h-96" />
+          <Spinner className="flex-1 max-h-96" />
         </Card>
         <Card className="col-span-full md:col-span-1 flex-col justify-between items-center">
           TODO...
@@ -19,4 +17,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default DashboardLoading;
