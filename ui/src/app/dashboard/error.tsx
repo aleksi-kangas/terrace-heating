9dd4dc4e90003825a52dc77c9e7d06d949f2cb71
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-06-23 10:33:42 +0300

    Add generic error handling to dashboard

---
+++ ui/src/app/dashboard/error.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import Card from '../components/card';
+import ErrorAlert from "../components/error_alert";
+
+const DashboardError = () => {
+  return (
+    <div className="flex flex-1 max-w-7xl">
+      <div className="flex-1 grid grid-cols-1 md:grid-cols-3 gap-4">
+        <Card className="col-span-full md:col-span-2 flex flex-col justify-evenly items-center">
+          <ErrorAlert message="Server connection failure" className="" />
+          <ErrorAlert message="Server connection failure" className="" />
+        </Card>
+        <Card className="col-span-full md:col-span-1 flex-col justify-between items-center">
+          TODO...
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardError;
