c2af639a03f9900856a1aca6d06fd92bf84d83c9
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-07-06 18:01:31 +0300

    Update schedules url

--- ui/src/app/api/heat_pump.ts
+++ ui/src/app/api/heat_pump.ts
@@ -1,11 +1,20 @@
 import {BoostingSchedule, BoostingScheduleVariable} from '@/app/api/types';
 
-const baseUrl = 'http://localhost:8000/heat_pump';
+const baseUrl = 'http://localhost:8000/heat-pump';
 
 export const fetchBoostingSchedule = async (
   boostingScheduleVariable: BoostingScheduleVariable
 ): Promise<BoostingSchedule> => {
-  const url = `${baseUrl}/schedules/` + boostingScheduleVariable;
+  let boostingScheduleVariableUrl: string | undefined = undefined;
+  switch (boostingScheduleVariable) {
+    case BoostingScheduleVariable.Circuit3:
+      boostingScheduleVariableUrl = 'circuit3';
+      break;
+    case BoostingScheduleVariable.LowerTank:
+      boostingScheduleVariableUrl = 'lower-tank';
+      break;
+  }
+  const url = `${baseUrl}/schedules/${boostingScheduleVariableUrl}`;
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error();
