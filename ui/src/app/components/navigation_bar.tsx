4ceee07daa9814492461738ce7649eef50e6a18b
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-07-15 16:21:29 +0300

    Extract NavigationBar as a component and fix handling of active path

---
+++ ui/src/app/components/navigation_bar.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTemperatureHalf} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+
+const navigation = [
+  {label: 'Dashboard', href: '/dashboard'},
+  {label: 'Graphs', href: '/graphs/external'},
+  {label: 'Schedules', href: '/schedules'},
+];
+
+interface NavigationBarItemProps {
+  active: boolean;
+  href: string;
+  label: string;
+}
+
+const NavigationBarItem = ({active, href, label}: NavigationBarItemProps) => {
+  return (
+    <Link
+      key={label}
+      href={href}
+      className={'rounded-md px-3 py-2 text-sm font-medium '.concat(
+        active
+          ? 'bg-gray-900 text-white'
+          : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+      )}
+    >
+      {label}
+    </Link>
+  )
+}
+
+const NavigationBar = () => {
+  const activeUrl = usePathname();
+  console.log(activeUrl);
+  return (
+    <header className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-4 sm:px6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <FontAwesomeIcon
+                icon={faTemperatureHalf}
+                className="h-8 w-8 text-gray-200"
+              />
+            </div>
+            <nav>
+              <div className="flex space-x-4 ml-6">
+                {navigation.map(item => (
+                  <NavigationBarItem
+                    key={item.label}
+                    active={item.href === activeUrl}
+                    href={item.href}
+                    label={item.label}
+                  />
+                ))}
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default NavigationBar;
