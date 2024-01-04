df15ffd6292eb6d8650d55a50fc8f9981b3cd19c
Aleksi Kangas <aleksi.kangas@hotmail.com>
2024-01-04 17:40:16 +0200

    Use Swagger also in production

--- heating_gateway/src/HeatingGateway.API/Program.cs
+++ heating_gateway/src/HeatingGateway.API/Program.cs
@@ -12,10 +12,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) {
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 app.MapControllers();
