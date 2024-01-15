5854f220cfa390909824873fd689fbfd1393884d
Aleksi Kangas <aleksi.kangas@hotmail.com>
2024-01-15 20:57:58 +0200

    Filter compressor usages once

--- ui/src/app/dashboard/compressor_usage_graph.tsx
+++ ui/src/app/dashboard/compressor_usage_graph.tsx
@@ -5,6 +5,7 @@ import Spinner from '@/app/components/spinner';
 
 const CompressorUsageGraph = async () => {
   const records = await fetchCompressorRecordsDays(2);
+  const compressorUsageRecords = records.filter(r => r.usage !== null);
   const xLimits =
     records.length > 0
       ? [records[0].time, records[records.length - 1].time]
@@ -15,8 +16,8 @@ const CompressorUsageGraph = async () => {
     <Suspense fallback={<Spinner className="flex-1 max-h-[45%] w-full" />}>
       <Graph
         className="flex-1 max-h-[45%] w-full"
-        dateTimes={records.map(r => r.time)}
-        values={records.filter(r => r.usage !== null).map(r => r.usage! * 100)}
+        dateTimes={compressorUsageRecords.map(r => r.time)}
+        values={compressorUsageRecords.map(r => r.usage! * 100)}
         label="Compressor Usage %"
         xLimits={xLimits}
         yLimits={yLimits}
